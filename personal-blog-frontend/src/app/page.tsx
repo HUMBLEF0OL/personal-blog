d05499f5a18e294d2129511b03464b8ce3b2f409
AMRAN9 <amit.rana2@ingka.com>
2024-11-11 10:51:55 +0530

    fix: api fixes for article fetch

--- personal-blog-frontend/src/app/page.tsx
+++ personal-blog-frontend/src/app/page.tsx
@@ -5,10 +5,11 @@ import ArticleCard from "@/components/ArticleCard";
 import { ArticleDataType } from "@/interface/article";
 
 export default async function Home() {
-  let articles = {};
+  let articles = [];
   try {
     const resp = await fetch("http://localhost:5000/article/all");
     articles = await resp.json();
+    console.log("articles are: ", articles)
   } catch (err) {
     console.log("no articles exists");
   }
